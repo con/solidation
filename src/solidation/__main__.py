2f41c1960e46da4eca5206f3e76051d740bc3e57
John T. Wodder II <git@varonathe.org>
2023-02-15 11:55:42 -0500

    Various code improvements

--- src/solidation/__main__.py
+++ src/solidation/__main__.py
@@ -1,11 +1,19 @@
-from datetime import datetime
+from __future__ import annotations
+from collections import Counter
+from collections.abc import Iterable
+from dataclasses import InitVar, dataclass, field
+from datetime import datetime, timedelta, timezone
 import logging
 import os
 from pathlib import Path
-from typing import List, Optional
+from random import sample
+from statistics import quantiles
+from typing import Any, List
 import click
 from github import Github
-from pydantic import BaseModel, constr
+from github.Issue import Issue
+from github.PullRequest import PullRequest
+from pydantic import BaseModel, Field, constr
 from ruamel.yaml import YAML
 from . import __version__
 
@@ -17,279 +25,252 @@ GHRepo = constr(regex=r"^[-_A-Za-z0-9]+/[-_.A-Za-z0-9]+$")
 
 class Configuration(BaseModel):
     project: str = "Project"
+    recent_days: int = Field(default=7, ge=1)
     repositories: List[GHRepo]
     members: List[GHUser]
 
 
-class HealthReport(object):
-    def __init__(
-        self,
-        github_token,
-        project="Project",
-        basepath=None,
-        recent_days=7,
-        repos=None,
-        members=None,
-    ):
-        self.basepath = Path(basepath) if basepath else Path.cwd()
-        self.gh = Github(github_token)
-        self.issues_since = datetime.fromtimestamp(
-            datetime.utcnow().timestamp() - recent_days * 24 * 3600
-        )
-        self.project = project
-        self.repos = repos
-        self.open_prs = []
-        self.active_issues = []
-        self.active_prs = []
-        # open issues and PRs
-        self.open_ip = []
-        if members is None:
-            raise NotImplementedError("Pick up all members")
-            # something like flattening of
-            # members = list(set(sum(
-            #     (
-            #         m.login for m in self.gh.get_organization(o).get_members()
-            #         for o in set(r.split('/')[0] for r in repos)
-            #     ),
-            #     []
-            # )))
-        self.members = members
+@dataclass
+class Consolidator:
+    token: InitVar[str]
+    gh: Github = field(init=False)
+    config: Configuration
+    since: datetime = field(init=False)
 
-    def get_repo_status(self, repo):
-        retrieve = (
-            "full_name",
-            "size",
-            "stargazers_count",
-            "watchers_count",
-            "forks_count",
-            "open_issues_count",
-            "network_count",
-            "subscribers_count",
+    def __post_init__(self, token: str) -> None:
+        self.gh = Github(token)
+        self.since = datetime.now(timezone.utc) - timedelta(
+            DAYS=self.config.recent_days
         )
-        props = {k: getattr(repo, k) for k in retrieve}
-        return props
 
-    def proc_repo(self, name):
-        log.info("Processing repo %s", name)
-        repo = self.gh.get_repo(name)
-        # basic status scores
-        rstatus = self.get_repo_status(repo)
-        # we already have open issue count, get open PRs too
-        open_prs = repo.get_pulls(state="open")
-        rstatus["open_prs_count"] = open_prs.totalCount
-        self.repostats[name] = rstatus
-        # write to a JSON file per repo
-        # the git history will have the longitudinal change wrt these scores
-        # status_dir = self.basepath / 'status'
-        # status_dir.mkdir(parents=True, exist_ok=True)
-        # with (status_dir / (name.replace('/', '__') + '.json')).open('w') as f:
-        #    json.dump(rstatus, f)
-        active_ip = repo.get_issues(state="all", since=self.issues_since)
-        active_issues = (i for i in active_ip if i.pull_request is None)
-        active_prs = (i for i in active_ip if i.pull_request)
-        open_issuesprs = repo.get_issues(state="open")
-        return dict(
+    def run(self) -> Report:
+        repostats = {}
+        open_prs = []
+        active_issues = []
+        active_prs = []
+        open_ip = []
+        for rn in self.config.repositories:
+            details = self.process_repo(rn)
+            self.repostats[rn] = details
+            self.open_prs.extend(details.open_prs)
+            self.active_issues.extend(details.active_issues)
+            self.active_prs.extend(details.active_prs)
+            self.open_ip.extend(details.open_ip)
+        return Report(
+            config=self.config,
+            repostats=repostats,
             open_prs=open_prs,
-            active_prs=active_prs,
             active_issues=active_issues,
-            open_ip=open_issuesprs,
+            active_prs=active_prs,
+            open_ip=open_ip,
         )
 
-    def get_issue_commenter_count(self):
-        commenters = {}
+    def process_repo(self, reponame: str) -> RepoDetails:
+        log.info("Processing repo %s", reponame)
+        repo = self.gh.get_repo(reponame)
+        active_ip = repo.get_issues(state="all", since=self.since)
+        return RepoDetails(
+            full_name=repo.full_name,
+            size=repo.size,
+            stargazers_count=repo.stargazers_count,
+            watchers_count=repo.watchers_count,
+            forks_count=repo.forks_count,
+            open_issues_count=repo.open_issues_count,
+            network_count=repo.network_count,
+            subscribers_count=repo.subscribers_count,
+            open_prs=list(repo.get_pulls(state="open")),
+            active_prs=[i for i in active_ip if i.pull_request],
+            active_issues=[i for i in active_ip if i.pull_request is None],
+            open_ip=list(repo.get_issues(state="open")),
+        )
+
+
+@dataclass
+class RepoDetails:
+    full_name: str
+    size: int
+    stargazers_count: int
+    watchers_count: int
+    forks_count: int
+    open_issues_count: int
+    network_count: int
+    subscribers_count: int
+    open_prs: list[PullRequest]
+    active_prs: list[Issue]
+    active_issues: list[Issue]
+    open_ip: list[Issue]
+
+    @property
+    def open_prs_count(self) -> int:
+        return len(self.open_prs)
+
+
+@dataclass
+class Report:
+    config: Configuration
+    repostats: dict[str, RepoDetails]
+    open_prs: list[PullRequest]
+    active_issues: list[Issue]
+    active_prs: list[Issue]
+    # Open issues and PRs:
+    open_ip: list[Issue]
+
+    def get_issue_commenter_count(self) -> Counter[str]:
+        commenters = Counter()
         for i in self.active_issues:
             for c in i.get_comments():
-                if self.issues_since > c.created_at:
+                if self.since > ensure_aware(c.created_at):
                     # does not fall into the reporting window
                     continue
-                count = commenters.get(c.user.login, 0)
-                commenters[c.user.login] = count + 1
+                commenters[c.user.login] += 1
         return commenters
 
-    def main(self):
-        # to aggregate issues across all repos
-        self.repostats = {}
-        self.open_prs = []
-        self.active_issues = []
-        self.active_prs = []
-        self.open_ip = []
-        for rn in self.repos:
-            props = self.proc_repo(rn)
-            self.open_prs.extend(props["open_prs"])
-            self.active_issues.extend(props["active_issues"])
-            self.active_prs.extend(props["active_prs"])
-            self.open_ip.extend(props["open_ip"])
+    def to_markdown(self) -> str:
+        now = datetime.now(timezone.utc)
+        dayscovered = self.config.recent_days
+        since = now - timedelta(DAYS=dayscovered)
 
-    def render_matrix_summary(self):
-        import statistics
-
-        now = datetime.utcnow()
-        dayscovered = (now - self.issues_since).days
-
-        print(f"#### {self.project} Health Update")
-        print("##### Covered projects (PRs/issues/stars/watchers/forks)")
-        print(
+        s = f"#### {self.config.project} Health Update\n"
+        s += "##### Covered projects (PRs/issues/stars/watchers/forks)\n"
+        s += (
             "; ".join(
                 f"[{k.split('/')[-1].replace('datalad-', 'dl-')}]"
                 f"(https://github.com/{k})"
-                f" ([{v['open_prs_count']}](https://github.com/{k}/pulls)/"
-                f"[{v['open_issues_count']}](https://github.com/{k}/issues)/"
-                f"{v['stargazers_count']}/{v['subscribers_count']}/{v['forks_count']})"
+                f" ([{v.open_prs_count}](https://github.com/{k}/pulls)/"
+                f"[{v.open_issues_count}](https://github.com/{k}/issues)/"
+                f"{v.stargazers_count}/{v.subscribers_count}/{v.forks_count})"
                 for k, v in self.repostats.items()
             )
-        )
+        ) + "\n"
 
-        non_dl_issues = [
+        outsider_issues = [
             i
             for i in self.active_issues
             if i.state == "open" and i.user.login not in self.members
         ]
-        if non_dl_issues:
-            print(
-                f"##### Non-{self.project} member issues active/opened in the last "
-                f"{dayscovered} days"
+        if outsider_issues:
+            s += (
+                f"##### Non-{self.config.project} member issues active/opened"
+                f" in the last {dayscovered} days\n"
             )
-            for i in sorted(non_dl_issues, key=lambda x: x.number):
-                age = now - i.created_at
-                print(
+            for i in sorted(outsider_issues, key=lambda x: x.number):
+                s += (
                     f"- [{i.title}]({i.html_url}) by {i.user.name}"
-                    f" [{i.repository.full_name}]"
+                    f" [{i.repository.full_name}]\n"
                 )
 
         recently_opened_issues = [
-            i for i in self.active_issues if i.created_at >= self.issues_since
+            i for i in self.active_issues if ensure_aware(i.created_at) >= since
         ]
         if recently_opened_issues:
-            print(
-                "##### Issues opened in last "
-                f"{dayscovered} days: {len(recently_opened_issues)}"
+            s += (
+                f"##### Issues opened in the last {dayscovered} days:"
+                f" {len(recently_opened_issues)}\n"
             )
+
         untriaged_issues = [
             i
             for i in self.active_issues
             if i.state == "open" and i.comments < 1 and not i.labels
         ]
         if untriaged_issues:
-            print("##### Untriaged issues of the last " f"{dayscovered} days")
+            s += "##### Untriaged issues of the last {dayscovered} days\n"
             for i in sorted(untriaged_issues, key=lambda x: x.created_at):
-                age = now - i.created_at
-                print(f"- [{i.title}]({i.html_url}) [{i.repository.full_name}]")
+                s + +f"- [{i.title}]({i.html_url}) [{i.repository.full_name}]\n"
 
-        print(
+        s += (
             f"##### Max 10 oldest, open, non-draft PRs ({len(self.open_prs)}"
-            " PRs open in total)"
+            " PRs open in total)\n"
         )
-        pr_count = 10
         for pr in sorted(
             (p for p in self.open_prs if not p.draft), key=lambda x: x.created_at
-        ):
-            pr_count -= 1
-            age = now - pr.created_at
-            print(f"- [{pr.title}]({pr.html_url}) ({age.days} days)")
-            if not pr_count:
-                break
-        n_random_ip = min(5, len(self.open_ip))
+        )[:10]:
+            age = now - ensure_aware(pr.created_at)
+            s += f"- [{pr.title}]({pr.html_url}) ({age.days} days)\n"
+
+        open_issues = [i for i in self.open_ip if i.pull_request is None]
+        n_random_ip = min(5, len(open_issues))
         if n_random_ip:
-            print(
+            s += (
                 f"##### {n_random_ip} random open issues to fix (of a total of"
-                f" {len(self.open_ip)})"
+                f" {len(open_issues)})\n"
             )
-            from random import shuffle
+            for i in sample(open_issues, n_random_ip):
+                age = now - ensure_aware(i.created_at)
+                s += f"- [{i.title}]({i.html_url}) ({age.days} days old)\n"
 
-            idx = list(range(len(self.open_ip)))
-            shuffle(idx)
-            count = 0
-            while n_random_ip:
-                i = self.open_ip[idx[count]]
-                if i.pull_request is not None:
-                    count += 1
-                    continue
-                print(
-                    f"- [{i.title}]({i.html_url}) ({(now - i.created_at).days}"
-                    " days old)"
-                )
-                n_random_ip -= 1
-                count += 1
-
-        print(
+        s += (
             f"##### Active issues in the past {dayscovered} days: "
             f"{len(self.active_issues)} "
-            f"({len(self.active_issues) / len(self.open_ip) * 100:.0f}%)"
+            f"({len(self.active_issues) / len(self.open_ip) * 100:.0f}%)\n"
         )
-        print(
-            "- Commenters:",
-            ", ".join(
-                [
-                    f"{k[0]} ({k[1]})"
-                    for k in sorted(
-                        self.get_issue_commenter_count().items(),
-                        key=lambda x: x[1],
-                        reverse=True,
-                    )
-                ]
-            ),
+        s += (
+            "- Commenters: "
+            + ", ".join(
+                f"{user} ({qty})"
+                for user, qty in self.get_issue_commenter_count().most_common()
+            )
+            + "\n"
         )
+
         recent_closed_issues = [i for i in self.active_issues if i.state == "closed"]
         if recent_closed_issues:
-            print(
+            s += (
                 f"##### Issues closed in the past {dayscovered} days:"
-                f" {len(recent_closed_issues)}"
+                f" {len(recent_closed_issues)}\n"
             )
             closed_age = [
                 (i.closed_at - i.created_at).days for i in recent_closed_issues
             ]
-            print(f"- Age quantiles (days): {statistics.quantiles(closed_age)}")
-            print(
-                "- Closed by:",
-                ", ".join(
-                    [
-                        f"{k[0]} ({k[1]})"
-                        for k in sorted(
-                            get_by_counts(recent_closed_issues, "closed_by").items(),
-                            key=lambda x: x[1],
-                            reverse=True,
-                        )
-                    ]
-                ),
+            s += f"- Age quantiles (days): {quantiles(closed_age)}\n"
+            s += (
+                "- Closed by: "
+                + ", ".join(
+                    f"{user} ({qty})"
+                    for user, qty in get_by_counts(
+                        recent_closed_issues, "closed_by"
+                    ).most_common()
+                )
+                + "\n"
             )
         recent_closed_prs = [
             i.as_pull_request() for i in self.active_prs if i.state == "closed"
         ]
         if recent_closed_prs:
-            print(
+            s += (
                 f"##### PRs completed in the past {dayscovered} days:"
-                f" {len(recent_closed_prs)}"
+                f" {len(recent_closed_prs)}\n"
             )
 
         merged_prs = [i for i in recent_closed_prs if i.merged_at]
         if merged_prs:
             for label, attr in (("Proposed", "user"), ("Merged", "merged_by")):
-                print(
-                    f"- {label} by:",
-                    ", ".join(
-                        [
-                            f"{k[0]} ({k[1]})"
-                            for k in sorted(
-                                get_by_counts(merged_prs, attr).items(),
-                                key=lambda x: x[1],
-                                reverse=True,
-                            )
-                        ]
-                    ),
+                s += (
+                    f"- {label} by: "
+                    + ", ".join(
+                        f"{user} ({qty})"
+                        for user, qty in get_by_counts(merged_prs, attr).most_common()
+                    )
+                    + "\n"
                 )
             pr_durations = [(i.merged_at - i.created_at).days for i in merged_prs]
-            print(
-                f"- PR duration quantiles (days): {statistics.quantiles(pr_durations)}"
-            )
+            s += f"- PR duration quantiles (days): {quantiles(pr_durations)}\n"
+            return s
 
 
-def get_by_counts(iterable, attr):
-    counts = {}
+def get_by_counts(iterable: Iterable[Any], attr: str) -> Counter[str]:
+    counts = Counter()
     for i in iterable:
         name = getattr(i, attr).login
-        count = counts.get(name, 0)
-        counts[name] = count + 1
+        counts[name] += 1
     return counts
+
+
+def ensure_aware(dt: datetime) -> datetime:
+    # Pygithub returns naïve datetimes for timestamps with a "Z" suffix.  Until
+    # that's fixed <https://github.com/PyGithub/PyGithub/pull/1831>, we need to
+    # make such datetimes timezone-aware manually.
+    return dt.replace(tzinfo=timezone.utc) if dt.tzinfo is None else dt
 
 
 @click.command()
@@ -301,32 +282,18 @@ def get_by_counts(iterable, attr):
     help="Read configuration from the given file",
     show_default=True,
 )
-@click.argument(
-    "basepath",
-    type=click.Path(exists=True, file_okay=False, writable=True, path_type=Path),
-    required=False,
-)
-def main(config: Path, basepath: Optional[Path]) -> None:
+def main(config: Path) -> None:
     logging.basicConfig(
-        format="%(asctime)s [%(levelname)-8s] %(name)s %(message)s",
+        format="%(asctime)s [%(levelname)-8s] %(name)s: %(message)s",
         datefmt="%Y-%m-%dT%H:%M:%S%z",
         level=logging.INFO,
     )
     log.info("solidation %s", __version__)
     with config.open() as fp:
         cfg = Configuration.parse_obj(YAML(typ="safe").load(fp))
-    dh = HealthReport(
-        os.environ["GITHUB_TOKEN"],
-        basepath=basepath,
-        project=cfg.project,
-        repos=cfg.repositories,
-        members=cfg.members,
-    )
-    dh.main()
-    dh.render_matrix_summary()
-    # print('\n\n')
-    # rl = dh.gh.get_rate_limit()
-    # pprint(rl.raw_data['core'])
+    cs = Consolidator(token=os.environ["GITHUB_TOKEN"], config=cfg)
+    report = cs.run()
+    print(report.to_markdown(), end="")
 
 
 if __name__ == "__main__":
